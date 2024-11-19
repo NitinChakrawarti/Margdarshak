734e18271311399027486c02074260871eb754cf
Nitin Chakrawarti <149225785+NitinChakrawarti@users.noreply.github.com>
2024-11-19 22:16:55 +0530

    persist storage

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,11 +1,26 @@
-import {configureStore} from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import chatReducer from '../features/chat/chatSlice';
 import todoReducer from '../features/chat/todo';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer } from 'redux-persist';
+import { combineReducers } from '@reduxjs/toolkit';
+// import { version } from 'react';
+
+
+const persistConfig = {
+    key: "root",
+    version: 1,
+    storage
+}
+
+const reducer = combineReducers({
+    mentors: chatReducer,
+    todos: todoReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, reducer);
 
 export const store = configureStore({
-    reducer: {
-        mentors: chatReducer,
-        todos: todoReducer,
-    },
+    reducer: persistedReducer
 
 })
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,9 +4,14 @@ import App from './App.jsx'
 import { Provider } from 'react-redux'
 import { store } from './app/store'
 import { Auth0Provider } from "@auth0/auth0-react";
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistStore } from 'redux-persist'
 
+let persistor = persistStore(store);
 
 createRoot(document.getElementById('root')).render(
+
+
   <Auth0Provider
     domain={import.meta.env.VITE_DOMAIN}
     clientId={import.meta.env.VITE_CLIENT_ID}
@@ -15,7 +20,9 @@ createRoot(document.getElementById('root')).render(
     }}
   >
     <Provider store={store}>
-      <App />
+      <PersistGate persistor={persistor} >
+        <App />
+      </PersistGate>
     </Provider>
   </Auth0Provider>,
 )
